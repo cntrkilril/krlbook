ad0ed0ca6bd35627dc73d03eced044047c1c9e75
cntrkilril <shalkir2002@mail.ru>
2021-06-14 14:28:46 +0300

    закончен модуль profile и posts, продолжаетсся разработка relationship

---
+++ krlbook/posts/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from .models import Post, Comment
+
+class PostModelForm(forms.ModelForm):
+    content = forms.CharField(widget=forms.Textarea(attrs={'rows':2}))
+    class Meta:
+        model = Post
+        fields = ('content', 'image')
+
+
